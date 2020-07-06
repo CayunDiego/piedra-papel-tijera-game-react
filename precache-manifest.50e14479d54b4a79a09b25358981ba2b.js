2f3058998272e80918a22bc2ad3c0467685d49b7
DiegoCayun <peridiegoc@hotmail.com>
2020-07-06 01:35:30 -0300

    Updates

---
+++ precache-manifest.50e14479d54b4a79a09b25358981ba2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa2daf5cdeff4e1e7cf1349fc063948",
+    "url": "/piedra-papel-tijera-game-react/index.html"
+  },
+  {
+    "revision": "f288d0cb02c99d2299ed",
+    "url": "/piedra-papel-tijera-game-react/static/css/main.2be837b8.chunk.css"
+  },
+  {
+    "revision": "971c3cb5429d4103331a",
+    "url": "/piedra-papel-tijera-game-react/static/js/2.dac6e1f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/piedra-papel-tijera-game-react/static/js/2.dac6e1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f288d0cb02c99d2299ed",
+    "url": "/piedra-papel-tijera-game-react/static/js/main.16f59c0d.chunk.js"
+  },
+  {
+    "revision": "a56d9d2968de3727086f",
+    "url": "/piedra-papel-tijera-game-react/static/js/runtime-main.203ea9a2.js"
+  }
+]);
